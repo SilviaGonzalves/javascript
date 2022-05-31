da32eb0423edc445a40c5663d9557ce36594157e
Silvia Gonzalves <sgonzal0@hotmail.com>
2022-05-31 15:35:35 -0300

    primer desafio entregable

--- script.js
+++ script.js
@@ -1,43 +1,67 @@
 
+// Objetivo: 
+// 1- pide un código de producto
+// 2- verifica que esté entre 1 y 4, si es otro código distinto de 9,muestra error
+// 3- pide que se ingrese la cantidad del producto
+// 4- se va acumulando hasta que ingresa 9 para terminar
+// 5- se muestra el total y las distintas formas de pago.
 
-//------------- primer ejemplo------------------------------------------------
 
-let nro = 0
-let suma = 0
 
-for (i = 1; i <= 7; i++){
-     nro = Number(prompt("Ingrese un numero : "))
-     suma = suma + nro
-     console.log("El numero ingresado es : " + nro + "  El acumulado es : " + suma)
+let producto = 0,
+    cantidad = 0,
+    total = 0,
+    ctdo = 0,
+    tC = 0,
+    sC = 0
+
+let precios= [100,150,200,250]
+
+
+producto = Number (prompt(`Para seleccionar el producto 1, ingrese : 1
+Para seleccionar el producto 2, ingrese : 2
+Para seleccionar el producto 3, ingrese : 3
+Para seleccionar el producto 4, ingrese : 4
+Para terminar,ingrese : 9 `))
+
+for (;producto !== 9;){
+     if (producto >= 1 && producto <= 4){
+        cantidad = Number(prompt("Ingrese la cantidad desea del producto"))
+        total = total + (precios[producto - 1] * cantidad)
+     }else{
+         console.log("Ingrese un producto válido")
+         
+     }
+     producto = Number (prompt(` Para seleccionar el producto 1, ingrese : 1
+     Para seleccionar el producto 2, ingrese : 2
+     Para seleccionar el producto 3, ingrese : 3
+     Para seleccionar el producto 4, ingrese : 4
+     Para terminar,ingrese : 9 `))
+} 
+if (total !== 0){  
+   alert("El total acumulado es : " + total)
+   ctdo = contado()
+   tC = tresCuotas()
+   sC = seisCuotas()
+
+   alert(`Seleccione su forma de pago :
+         Contado     (10%dto): ${ctdo}  
+         En 3 cuotas (s/int.): ${tC}
+         En 6 cuotas (+  20%): ${sC}`)
+   console.log("Fin del programa")
+}else{
+   console.log("Fin del programa")
+} 
+
+
+
+function contado(){
+   return (total - (total * 0.10))
 }
-
-
-
-//------------segundo ejemplo para desafio falta ingresar ESC
-   
-let nombre  
-let nombre1 = "Los nombres son : "
-  
-while (nombre !== "ESC"){
-       nombre = prompt("Ingrese un nombre : ")
-       if (nombre === "ESC"){
-           break
-      }else {
-           nombre1 = nombre1 + nombre
-           nombre1 = nombre1 + " "
-           console.log(nombre1) 
-       }
+function tresCuotas(){
+   return total / 3
 }
-
-
-//------------- tercer ejemplo ------------------------------------
-     
-let veces = Number(prompt("Ingrese la cantidad de veces a repetir"))
-    console.log(veces)
-let k = 0
-while (k < veces){
-     console.log("Hola");
-     console.log(" ");
-     k = k + 1;
+function seisCuotas(){
+   return (total * 1.20) / 6
 }
 
